cf3e341cc2af9706d3a517831a4dcb6a2d2fdfc0
glglvudals <glglvudals@gmail.com>
2024-01-15 14:56:32 +0900

    test

--- src/components/HeaderComponent.jsx
+++ src/components/HeaderComponent.jsx
@@ -12,8 +12,11 @@ export default class HeaderComponent extends Component {
         <header>
           <nav className="navbar navbar-expand-md navbar-dark bg-dark">
             <div>
-              <a href="http://www.fastcampus-dragon.net" className="navbar-brand">
-                kimdragon - jenkins
+              <a
+                href="http://www.fastcampus-dragon.net"
+                className="navbar-brand"
+              >
+                flagsheep - jenkins
               </a>
             </div>
           </nav>
